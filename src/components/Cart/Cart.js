32232487082370ca120a608ff7f51723f273845f
tvn0122 <ntat0013@gmail.com>
2021-03-06 03:50:11 +0700

    update shopping cart UI

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import CartItem from "./CartItem";
 import "./Cart.scss";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function Cart() {
   // const [cartItem, setCartItem] = useState([]);
@@ -13,17 +14,28 @@ function Cart() {
     }
   }, [itemList]);
 
+  const history = useHistory();
+  const handleContinueShopping = () => history.push("/");
+
   return (
     <div className="page">
       {itemList.itemList.length === 0 ? (
-        <div className="cart__empty">Cart is empty</div>
+        <div className="cart__empty">
+          <h1>Cart is currently empty</h1>
+          <h3>Look like you have no items in your shopping cart</h3>
+          <button onClick={handleContinueShopping}>Continue Shopping</button>
+        </div>
       ) : (
         <div className="cart__container">
           <div className="cart-item__container">
             <CartItem cartItem={itemList} />
           </div>
 
-          <div className="cart__total">Total{itemList.totalPrice}</div>
+          <div className="cart__total">
+            <h2>Cart Total</h2>
+
+            <span>Total: {itemList.totalPrice}</span>
+          </div>
         </div>
       )}
     </div>
