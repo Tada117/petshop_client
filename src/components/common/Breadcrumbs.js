3f0251428b6803a46909b30cb9251e5ef725b285
tvn0122 <ntat0013@gmail.com>
2021-03-10 02:33:37 +0700

    update UI

--- src/components/common/Breadcrumbs.js
+++ src/components/common/Breadcrumbs.js
@@ -5,6 +5,32 @@ import {
   Typography,
 } from "@material-ui/core";
 import { withRouter } from "react-router-dom";
+import { AiOutlineDoubleRight } from "react-icons/ai";
+
+const bcContainerStyle = {
+  border: 0,
+  height: 48,
+  maxWidth: "1344px",
+  width: "100%",
+  margin: "10px auto 0",
+  display: "flex",
+  alignItems: "center",
+  justifyContents: "center",
+  padding: "0 30px",
+  borderBottom: "0.25px solid #ccc",
+};
+
+const bcItem = {
+  textDecoration: "none",
+  cursor: "pointer",
+  color: "white",
+  fontSize: "16px",
+  fontWeight: "bold",
+  background: "rgb(243, 197, 190)",
+  borderRadius: "10px",
+  padding: "5px",
+};
+const bcLastItem = {};
 
 const Breadcrumbs = (props) => {
   const {
@@ -12,23 +38,27 @@ const Breadcrumbs = (props) => {
     location: { pathname },
   } = props;
   const pathnames = pathname.split("/").filter((x) => x);
-  console.log(pathnames);
-  console.log("history", history);
 
   return pathnames.length < 1 ? null : (
-    <MUIBreadcrumbs aria-label="breadcrumb">
-      {pathnames.length > 0 ? (
-        <Link onClick={() => history.push("/")}>Home</Link>
-      ) : (
-        <Typography> Home </Typography>
-      )}
+    <MUIBreadcrumbs
+      style={bcContainerStyle}
+      separator={<AiOutlineDoubleRight />}
+      aria-label="breadcrumb"
+    >
+      <Link onClick={() => history.push("/")} style={bcItem}>
+        {" "}
+        Home{" "}
+      </Link>
+
       {pathnames.map((name, index) => {
         const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
         const isLast = index === pathnames.length - 1;
         return isLast ? (
-          <Typography key={name}>{name}</Typography>
+          <Typography style={bcLastItem} key={name}>
+            {name}
+          </Typography>
         ) : (
-          <Link key={name} onClick={() => history.push(routeTo)}>
+          <Link style={bcItem} key={name} onClick={() => history.push(routeTo)}>
             {name}
           </Link>
         );
